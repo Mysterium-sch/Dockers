966c8f788ae3eb58e030a6b4262acf55d8c5a7dc
Lilian Lamb <57109743+Mysterium-sch@users.noreply.github.com>
2024-05-29 10:22:39 -0400

    updates

--- jetson/jetson2/launch.sh
+++ jetson/jetson2/launch.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 source /opt/ros/humble/setup.bash
 source /ws/install/setup.bash
-cd /ws/install
 ros2 launch ros2_bringup ros2_bringup.launch.py serial:="'20096894'" sonar:='true' device:='jetson_1' imu:="cv7"
 /bin/bash
