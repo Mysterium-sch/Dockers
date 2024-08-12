63e5eb3ef1b02d378f6dc42eb0f1f7b688e055c2
Lilian Lamb <57109743+Mysterium-sch@users.noreply.github.com>
2024-08-12 16:42:44 -0400

    update jetson

--- jetson/jetson2/launch.sh
+++ jetson/jetson2/launch.sh
@@ -2,5 +2,5 @@
 source /opt/ros/humble/setup.bash
 source /ws/install/setup.bash
 cd data
-ros2 launch ros2_bringup stereorig_1.launch.py namespace:="jetson_1"
+ros2 launch ros2_bringup stereorig.launch.py namespace:="jetson_1"
 /bin/bash
